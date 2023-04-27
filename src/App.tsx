17a5dc588a5c351799dcee6832f71b16e570b642
thebigbone <pacman@duck.com>
2023-04-27 18:27:10 +0530

    heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import { Button, Show, ButtonGroup, Grid, GridItem } from "@chakra-ui/react";
+import {
+  Button,
+  Show,
+  ButtonGroup,
+  Grid,
+  GridItem,
+  Heading,
+} from "@chakra-ui/react";
 import ColorSwitch from "./components/ColorSwitch";
 import GameGrid from "./components/GameGrid";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`, // for mobile devices
-        lg: `"nav nav" "aside main"`, // for larger devices
-      }}
-    >
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
-      <Show above="lg">
-        <GridItem area="aside"></GridItem>
-      </Show>
-      <GridItem area="main">
-        <GameGrid />
-      </GridItem>
-    </Grid>
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`, // for mobile devices
+          lg: `"nav nav" "aside main"`, // for larger devices
+        }}
+      >
+        <GridItem area="nav">
+          <NavBar />
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside"></GridItem>
+        </Show>
+        <GridItem area="main">
+          <GameGrid />
+        </GridItem>
+      </Grid>
+    </>
   );
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { Heading, HStack, Image } from "@chakra-ui/react";
 import React from "react";
 import logo from "../assets/logo.png";
 import ColorSwitch from "./ColorSwitch";
@@ -7,6 +7,9 @@ const NavBar = () => {
   return (
     <HStack justify="space-between" padding="10px">
       <Image src={logo} boxSize="100px"></Image>
+      <Heading color="blue.300" fontFamily="mono">
+        Gateway Game
+      </Heading>
       <ColorSwitch />
     </HStack>
   );
